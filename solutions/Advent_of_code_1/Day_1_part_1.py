c3e6c9e76f3c71ef6efe20b3686916a286244a95
sanrutas <saturnasv2@gmail.com>
2025-01-12 03:59:48 +0200

    renaming

--- solutions/Advent_of_code_1/Day_1_part_1.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-with open("inputs/input_day_1.txt", 'r') as textfile:
-    input = textfile.read().splitlines()
-
-df = pd.DataFrame([line.split() for line in input], columns=['Column1', 'Column2']).astype(int)
-for col in df:
-    df[col] = df[col].sort_values(ignore_index=True)
-df['Absolute_Difference'] = (df['Column1'] - df['Column2']).abs()
-print(df['Absolute_Difference'].sum())
